5a5701e093b59fdf9a4f53b724bbcae3769c497b
Don-Uwe <info@boebs-elektro.de>
2026-07-01 21:43:09 +0900

    chore: migrate ESLint to flat config and npm-compatible lint script

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,10 +2,18 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', 'coverage/*'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'react/display-name': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react-hooks/rules-of-hooks': 'off',
+      'import/no-unresolved': 'off',
+    },
   },
 ]);
